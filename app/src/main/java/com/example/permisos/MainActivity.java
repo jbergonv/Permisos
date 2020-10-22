167e5e3be008fe8a28d53671df9a3df118e0eb15
Jose <jbergon@iesflorenciopintado.es>
2020-10-22 14:04:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/permisos/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.permisos;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int VENGO_DE_LA_CAMARA = 1;
+    Button hacerFoto;
+    ImageView imageView;
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == VENGO_DE_LA_CAMARA && resultCode==RESULT_OK){
+
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+
+            imageView.setImageBitmap(bitmap);
+
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hacerFoto = findViewById(R.id.buttonFoto);
+        imageView = findViewById(R.id.imageView);
+
+        hacerFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                if(intent.resolveActivity(getPackageManager())!=null){
+
+                    startActivityForResult(intent,VENGO_DE_LA_CAMARA);
+
+                }
+                else{
+
+                    Toast.makeText(MainActivity.this,"Necesitas programa que haga fotos",Toast.LENGTH_LONG);
+                }
+            }
+        });
+
+
+
+
+
+    }
+}
